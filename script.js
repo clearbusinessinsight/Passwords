0d0adb88c3f1c4551a8e4c727bd5d97ee7249fd1
choltz <choltz@cacbydesign.com>
2022-08-04 14:07:11 -0400

    8.4.207

--- script.js
+++ script.js
@@ -6,82 +6,41 @@ var NumList = ["1","2","3","4","5","6","7","8","9"];
 var SpecList = ["!","@","#","$","%","&","*","?","*"];
 var newtext = " "
 
-
-//var password = generatePassword();
-//var passwordText = document.querySelector("#password");
-//var passwordText = "test"
- // passwordText.value = password;
-// ------------------------
-// Pass is the conbined string
-
-// return array[Math.floor(Math.random() * Pass.length)]
-// }
-// for (var i = 0; i< 8; i++) {
-// result += writePassword()
-// }
-// console.log(array);
-
-
-// let result = ' ';
-// const charactersLength = Pass.length;
-// for ( let i = 0; i <length; i++ ) {
-//   result += characters.charAt(Math.floor(Math.random() * charactersLength))
-// }
-
-// return result;
-
-// }
-
-
-
 //If Cancel incl_Upper will be false
 var incl_Upper = window.confirm("Press OK to include Upper case letters or Cancel to Skip.")
 if(incl_Upper === true) {
 
-  console.log(UpperList)
-} else {
+ } else {
   UpperList.length = 0
-  console.log("You pressed Cancel")
-  console.log(UpperList)
+
 }
 
 //If Cancel incl_Lpper will be false
 var incl_Lower = window.confirm("Press OK to include Lower case letters or Cancel to skip.")
 if(incl_Lower === true) {
   
-  console.log(LowerList)
   } else {
     LowerList.length = 0
-    console.log("You pressed Cancel")
-    console.log(LowerList)
   }
 
 //If Cancel incl_Num will be false
 var incl_Num = window.confirm("Press OK to include Numbers or Cancel to Skip.")
 if(incl_Num === true) {
   
-  console.log(NumList)
   } else {
     NumList.length = 0
-    console.log("You pressed Cancel")
   }
 
 //If Cancel incl_Spec will be false
 var incl_Spec = window.confirm("Press OK to include Special Characters or Cancel to skip.")
 if(incl_Spec === true) {
   
-  console.log(SpecList)
   } else {
     SpecList.length = 0
-    console.log("You pressed Cancel")
   }
 
 const Pass = UpperList.concat(LowerList).concat(NumList).concat(SpecList)
-//console.log(Pass)
 
-
- 
- 
  //Add event listener to generate button
  
  generateBtn.addEventListener("click", writePassword);
@@ -92,21 +51,14 @@ function writePassword(array) {
   //newtext is a string
 //let newtext = Pass.toString();
 return array[Math.floor(Math.random() * array.length)]
+
 }
 result = "";
 for (var i = 0; i < 8; i++) {
   result += writePassword(Pass)
 }
+
 console.log(result)
 
 
 
-
-
-
-
-
-  
-  //console.log(Pass)
-  // console.log(newtext);
-  
