6a402de6359b5cf50012772190fe82d5e08cc19f
choltz <choltz@cacbydesign.com>
2022-08-04 22:31:45 -0400

    last

--- script.js
+++ script.js
@@ -16,47 +16,57 @@ function writePassword(array){
 
     // Set the number of Characters
   var CharNum = window.prompt("How many Characters would you like?")
+  if(CharNum >= 128) {
+     CharNum = 128
+  }
+  if(CharNum < "8") {
+    CharNum = "8"
+  }
   
   //If Cancel incl_Upper will be false
   var incl_Upper = window.confirm("Press OK to include Upper case letters or Cancel to Skip.")
   if(incl_Upper) {
   Pass = UpperList.concat(Pass)
-
   }
+  console.log(Pass)
 
   //If Cancel incl_Lpper will be false
   var incl_Lower = window.confirm("Press OK to include Lower case letters or Cancel to skip.")
   if(incl_Lower) {
     //LowerList.length = 0
     Pass = LowerList.concat(Pass)
-    
-   
   }
 
   //If Cancel incl_Num will be false
   var incl_Num = window.confirm("Press OK to include Numbers or Cancel to Skip.")
   if(incl_Num) {
-    //NumList.length = 0
-    Pass = NumList.concat(Pass)
-    
+      Pass = NumList.concat(Pass)
   }
 
   //If Cancel incl_Spec will be false
   var incl_Spec = window.confirm("Press OK to include Special Characters or Cancel to skip.")
   if(incl_Spec) {
     Pass = SpecList.concat(Pass)
-   
   }
-var Newdata = Pass.toString();
-var stringWithCommas = ""
-var stringWithoutCommas = stringWithCommas.replace(/,/g, '');
-console. log(stringWithoutCommas);
-console.log(Newdata);
-var Newdatalength = Newdata.length;
-var result = " ";
+  
+
+  if(!incl_Upper) {
+    if(!incl_Lower) {
+      if(!incl_Num) {
+        if(!incl_Spec) {
+          Pass = ["A","B","c","d","!","@","#"]
+        }
+      }
+    }
+  }
+
+    console.log(Pass)
+
+var result = "";
   
 for (var i = 0; i < CharNum; i++) {
- result += Newdata.charAt(Math.floor(Math.random() * Newdatalength))
+ //result += Newdata.charAt(Math.floor(Math.random() * Newdatalength))
+ result += Pass[(Math.floor(Math.random() * Pass.length))]
  }
  document.getElementById("password").innerHTML = result;
 }
